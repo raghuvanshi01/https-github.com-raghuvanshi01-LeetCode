db7375dc09d7ef064eb5465af0baf3ca86414b6f
Gaurav Singh <109308730+raghuvanshi01@users.noreply.github.com>
2022-09-23 12:25:03 +0530

    Time: 3 ms (35.92%), Space: 5.8 MB (83.98%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int recur(int n,vector<int> &dp)
     {
         if(n<=1)
@@ -16,8 +17,24 @@ public:
         }
         return dp[n]=recur(n-1,dp)+recur(n-2,dp)+recur(n-3,dp);
     }
+    */
     int tribonacci(int n) {
-       vector<int> dp(n+1,-1);
-        return recur(n,dp);
+       int dp[n+1];
+        if(n<=1)
+        {
+            return n;
+        }
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        if(n<3)
+        {
+            return dp[n];
+        }
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
     }
 };
